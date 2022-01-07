aa512252562a7aa34b27b2dd040f52d0a90bdb4c
dreadwing5 <sachinspyk@outlook.com>
2022-01-07 18:51:45 +0530

    added login signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,21 +3,11 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", (req, res) => {
-  res.render("index", {
-    title: "Home Page",
-    username: "test",
-  });
+  res.render("login");
 });
 
-router.get("/register", (req, res) => {
+router.get("/sign-up", (req, res) => {
   res.render("sign-up");
-});
-
-// this route connect to the flask server
-router.get("/get-data", async (req, res) => {
-  const response = await axios.get("http://127.0.0.1:5000/flask");
-  const data = response.data;
-  console.log(data);
 });
 
 module.exports = router;
