941952888f578549f323c262768ba1a2fb0b4918
dreadwing5 <sachinspyk@outlook.com>
2022-01-08 12:47:01 +0530

    removed

--- src/routes/index.js
+++ src/routes/index.js
@@ -30,22 +30,4 @@ router.get("/maps", (req, res) => {
   res.render("google-maps");
 });
 
-router.get("/mybooking", (req, res) => {
-  const data = {
-    srclat: "12.95337133011648",
-    srclng: "77.68157958984376",
-    destlat: "12.9715987",
-    destlng: "77.5945627",
-    ownerid: "123@test1.com",
-    commuterid: "123@cmrit.ac.in",
-    vehicleid: "1",
-    paymentStatus: "Ok",
-    feedback: "Was a great , loved every bit of it",
-    date: "2022-01-08",
-    paymentID: "123",
-  };
-  res.render("mybooking", {
-    data,
-  });
-});
 module.exports = router;
